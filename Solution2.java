33689ce2e85591cb746d2fc5476a19c6853c678a
arnavmahajan0412 <65086191+arnavmahajan0412@users.noreply.github.com>
2021-08-05 13:47:03 +0530

    Delete Solution2.java

--- Solution2.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Solution2 {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
-        
-    }
-}
